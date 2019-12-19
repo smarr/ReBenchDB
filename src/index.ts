7c0b8ffeaa98b55e4abf29beadc1530e6a277968
Stefan Marr <git@stefan-marr.de>
2019-12-19 11:46:15 +0100

    Added basic index.ts to accept put requests
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import Koa from 'koa';
+import koaBody from 'koa-body';
+import Router from 'koa-router';
+import { Database } from './db';
+import { BenchmarkData } from './api';
+
+console.log('Starting up ReBenchDB');
+
+
+const dbConfig = {
+  user: process.env.RDB_USER || '',
+  password: process.env.RDB_PASS || '',
+  host: process.env.RDB_HOST || 'localhost',
+  database: process.env.RDB_DB || 'test_rdb3',
+  port: 5432
+};
+
+const port = process.env.PORT || 33333;
+
+
+const app = new Koa();
+const router = new Router();
+const db = new Database(dbConfig);
+
+
+router.get('/', async ctx => {
+  ctx.body = 'TODO';
+  ctx.type = 'text';
+});
+
+// curl -X PUT -H "Content-Type: application/json" -d '{"foo":"bar","baz":3}' http://localhost:33333/rebenchdb/results
+router.put('/rebenchdb/results', koaBody(), async ctx => {
+  const data: BenchmarkData = ctx.request.body;
+  try {
+    await db.recordData(data);
+    ctx.status = 200;
+  } catch (e) {
+    ctx.status = 500;
+    ctx.type = 'text';
+    ctx.body = `An error:
+${e.stack}`;
+  }
+});
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+console.log(`Starting server on localhost:${port}`);
+app.listen(port);
