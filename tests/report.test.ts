11aa0ea1be0cef3219392d89df37fa9377d0a3af
Stefan Marr <git@stefan-marr.de>
2020-09-03 18:23:44 +0100

    Abort execution with warning when data misses fitting entries for changed version
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/report.test.ts
+++ tests/report.test.ts
@@ -63,4 +63,51 @@ describe('Knitr Report Generation', () => {
       rmdirSync(imageFolder, { recursive: true });
     });
   });
+
+  describe('Report after renaming executables', () => {
+    const outputFileWithoutExtension = `report.test.ts`;
+    const outputFile = `${outputFileWithoutExtension}.html`;
+
+    it('Should successful execute generation', async () => {
+      const baseHash = '2b04e9f74ca0bc3e95d3c50e04f67a17b9536e8e';
+      const changeHash = '2ed3bac9e7de2bc0f7ebf70ad07066fb559f41d3';
+      const baseFile = `${__dirname}/data/renamed-exec-base.qs`;
+      const changeFile = `${__dirname}/data/renamed-exec-change.qs`;
+
+      const extraCmd = `from-file;${baseFile};${changeFile}`;
+
+      const output = await startReportGeneration(
+        baseHash,
+        changeHash,
+        outputFile,
+        {} as DatabaseConfig,
+        extraCmd
+      );
+
+      expect(output.code).toBe(0);
+    }, 60000);
+
+    it('Should indicate problem with data', () => {
+      const content: string = readFileSync(
+        `${__dirname}/../resources/reports/${outputFile}`,
+        'utf8'
+      );
+      expect(content).toEqual(
+        expect.stringContaining('Issue with Unexpected Data')
+      );
+
+      // and we assume the rest afterwards is not executed
+      expect(content).not.toEqual(
+        expect.stringContaining('Summary Over All Benchmarks')
+      );
+    });
+
+    afterAll(async () => {
+      unlinkSync(`${__dirname}/../resources/reports/${outputFile}`);
+      const imageFolder = `${__dirname}/../resources/reports/${getOutputImageFolder(
+        outputFile
+      )}`;
+      rmdirSync(imageFolder, { recursive: true });
+    });
+  });
 });
