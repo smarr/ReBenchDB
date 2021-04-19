76c6ae8f800e4e57c69867dc17ee255029913c82
Stefan Marr <git@stefan-marr.de>
2021-04-19 10:08:33 +0100

    Make regex more robust to various paths and R versions
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- resources/render.js
+++ resources/render.js
@@ -209,7 +209,7 @@ function renderBenchmarks(benchmarks, $) {
 function renderBenchmark(benchmark) {
   // capture the beginning of the path but leave the last element of it
   // this regex is also used in somns.Rmd, the suites part, for creating a table
-  const pathRegex = /^(.*?)((?:\/\w+)\s.*$)/;
+  const pathRegex = /^([^\s]*)((?:\/\w+)\s.*$)/;
   const cmdline = benchmark.cmdline.replace(pathRegex, '.$2');
 
   const plotId =
