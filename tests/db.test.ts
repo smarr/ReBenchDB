d8faec8a713b186dd66feed22f44925d28db534c
Stefan Marr <git@stefan-marr.de>
2022-07-31 15:22:04 +0100

    Implement test setup, just creating the database and data for tests
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/db.test.ts
+++ tests/db.test.ts
@@ -1,4 +1,63 @@
+import {
+  TestDatabase,
+  createAndInitializeDB,
+  closeMainDb
+} from './db-testing.js';
+import { BenchmarkData } from '../src/api.js';
+import { readFileSync } from 'fs';
+import { getDirname } from '../src/util.js';
+
+const __dirname = getDirname(import.meta.url);
+
 describe('Timeline-plot Queries', () => {
+  let db: TestDatabase;
+  let projectName: string;
+  let baseBranch: string;
+  let changeBranch: string;
+
+  let earlierBaseCommitId: string;
+  let baseCommitId: string;
+  let changeCommitId: string;
+
+  beforeAll(async () => {
+    db = await createAndInitializeDB('db_ts_basic');
+
+    const data = readFileSync(`${__dirname}/small-payload.json`).toString();
+    const basicTestData: BenchmarkData = JSON.parse(data);
+    projectName = basicTestData.projectName;
+
+    baseBranch = basicTestData.source.branchOrTag = 'base-branch';
+    db.setProjectBaseBranch(projectName, basicTestData.source.branchOrTag);
+    earlierBaseCommitId = basicTestData.source.commitId;
+
+    await db.recordMetaDataAndRuns(basicTestData);
+    await db.recordAllData(basicTestData);
+
+    // have a second experiment in the database
+    basicTestData.experimentName += ' 2';
+    basicTestData.startTime = '2019-12-14T22:49:56';
+    changeBranch = basicTestData.source.branchOrTag = 'change-branch';
+    changeCommitId = basicTestData.source.commitId =
+      '2222222222222222222222222222222222222222';
+
+    await db.recordMetaDataAndRuns(basicTestData);
+    await db.recordAllData(basicTestData);
+
+    // have a merge in the database
+    basicTestData.experimentName += ' 3';
+    basicTestData.startTime = '2019-12-15T22:49:56';
+    basicTestData.source.branchOrTag = baseBranch;
+    baseCommitId = basicTestData.source.commitId =
+      '3333333333333333333333333333333333333333';
+
+    await db.recordMetaDataAndRuns(basicTestData);
+    await db.recordAllData(basicTestData);
+  });
+
+  afterAll(async () => {
+    await db.close();
+  });
+
   describe('Retrieving branch names based on commit ids', () => {
     it.todo('should return `null` if there is an error');
 
@@ -14,4 +73,8 @@ describe('Timeline-plot Queries', () => {
 
     it.todo('should identify the current data points per branch');
   });
+});
+
+afterAll(() => {
+  closeMainDb();
 });
