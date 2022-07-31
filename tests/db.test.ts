d7d10f1c0901161ac376883a05770daa2fa0943d
Stefan Marr <git@stefan-marr.de>
2022-07-31 11:07:59 +0100

    Added todos for writing tests
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

---
+++ tests/db.test.ts
@@ -0,0 +1,17 @@
+describe('Timeline-plot Queries', () => {
+  describe('Retrieving branch names based on commit ids', () => {
+    it.todo('should return `null` if there is an error');
+
+    it.todo('should handle both commit ids being on the same branch');
+
+    it.todo('should attribute branch names correctly to base and change');
+  });
+
+  describe('Retrieving timeline data', () => {
+    it.todo('should return `null` if there is an error');
+
+    it.todo('should return median and BCIs for each branch');
+
+    it.todo('should identify the current data points per branch');
+  });
+});
