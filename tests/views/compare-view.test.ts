5b3523e35c8ea88d748b753d77969f5fa7828329
Stefan Marr <git@stefan-marr.de>
2023-02-26 10:49:07 +0000

    Move expected HTML results for tests into HTML files
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- tests/views/compare-view.test.ts
+++ tests/views/compare-view.test.ts
@@ -1,4 +1,4 @@
-/* eslint-disable max-len */
+import { readFileSync } from 'fs';
 import { prepareTemplate } from '../../src/templates.js';
 import * as numFormat from '../../src/data-format.js';
 import * as viewHelpers from '../../src/views/helpers.js';
@@ -8,6 +8,13 @@ import {
   CompareStatsRowAcrossVersions,
   StatsSummary
 } from 'views/view-types.js';
+import { robustPath } from '../../src/util.js';
+
+function loadResult(name: string): string {
+  return readFileSync(
+    robustPath(`../tests/views/expected-results/${name}.html`)
+  ).toString();
+}
 
 describe('Compare View Parts', () => {
   describe('Statistics in Row for Comparison Across Versions', () => {
@@ -22,14 +29,7 @@ describe('Compare View Parts', () => {
         ...viewHelpers
       };
       const result = tpl(data);
-      expect(result).toEqual(`<td class="stats-samples">43</td>
-<td><span class="stats-median" title="median">0.33</span></td>
-<td><span class="stats-change" title="change over median run time"></span></td>
-<td><span class="stats-median" title="median">0</span></td>
-<td><span class="stats-gc-change" title="change over median GC time">54600</span></td>
-<td><span class="stats-median" title="median">222b</span></td>
-<td><span class="stats-alloc-change" title="change over median allocated memory">64600</span></td>
-`);
+      expect(result).toEqual(loadResult('version-stats'));
     });
   });
 
@@ -56,72 +56,7 @@ describe('Compare View Parts', () => {
         ...viewHelpers
       };
       const result = tpl(data);
-      expect(result).toEqual(`
-<td class="stats-samples">
-
-
-ast 43
-
-<br>
-bc 12
-
-</td>
-<td><span class="stats-median" title="median">
-
-
-0.33
-
-<br>
-0.45
-
-</span></td>
-<td>
-
-
-0.33
-<span class="stats-change" title="change over median run time">54600</span>
-
-<br>
-0.45
-<span class="stats-change" title="change over median run time">3400</span>
-
-</td>
-<td><span class="stats-median" title="median">
-
-
-0
-
-<br>
-0
-
-</span></td>
-<td>
-
-
-<span class="stats-change" title="change over median GC time">54600</span>
-
-<br>
-<span class="stats-change" title="change over median GC time">232300</span>
-
-</td>
-<td><span class="stats-median" title="median">
-
-
-222b
-
-<br>
-675b
-
-</span></td>
-<td>
-
-
-<span class="stats-change" title="change over median allocated">64600</span>
-
-<br>
-<span class="stats-change" title="change over median allocated">604600</span>
-
-</td>`);
+      expect(result).toEqual(loadResult('exec-stats'));
     });
   });
 
@@ -166,18 +101,7 @@ bc 12
       };
 
       const result = tpl(data);
-      expect(result)
-        .toEqual(`<button type="button" class="btn btn-sm btn-cmdline btn-popover"
-data-content="<code>som/some-command with args</code>"></button>
-
-<button type="button" class="btn btn-sm btn-environment btn-popover"
-data-content="MyHost | Linux | 121kb | Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz | 3GHz"></button>
-
-
-
-<button type="button" class="btn btn-sm btn-profile" data-content="123456/1/2,123457/3/4"></button>
-
-<button type="button" class="btn btn-sm btn-timeline" data-content='{"b":"my-benchmark","e":"exe1","s":"suite2"}'></button>`);
+      expect(result).toEqual(loadResult('button-info'));
     });
   });
 
@@ -194,18 +118,7 @@ data-content="MyHost | Linux | 121kb | Intel(R) Core(TM) i7-7700HQ CPU @ 2.80GHz
       };
 
       const result = tpl(data);
-      expect(result).toEqual(`<h2 id="overview">Result Overview</h2>
-<img src="some-url.svg">
-<dl class="row">
-<dt class="col-sm-3">Number of Run Configurations</dt>
-<dd class="col-sm-8">232</dd>
-<dt class="col-sm-3">Run time (geomean)</dt>
-<dd class="col-sm-8">0.5 (min. 0.1, max. 1.1)</dd>
-<dt class="col-sm-3">GC time (geomean)</dt>
-<dd class="col-sm-8">2.5 (min. 2.1, max. 3.1)</dd>
-<dt class="col-sm-3">Allocated bytes (geomean)</dt>
-<dd class="col-sm-8">4.5 (min. 4.1, max. 5.1)</dd>
-</dl>`);
+      expect(result).toEqual(loadResult('stats-summary'));
     });
   });
 });
