9f8c518f27b96c202ea179d2cc304d3c9b9a156e
Stefan Marr <git@stefan-marr.de>
2023-05-01 22:53:38 +0100

    Only log errors and above when we run unit tests

--- src/logging.ts
+++ src/logging.ts
@@ -3,10 +3,25 @@ import { Logger } from 'tslog';
 // 0: silly, 1: trace, 2: debug, 3: info, 4: warn, 5: error, 6: fatal
 const trace = 2;
 const info = 3;
+const error = 5;
 
-const minLevel = ('DEV' in process.env ? process.env.DEV === 'true' : false)
-  ? trace
-  : info;
+function getLoggingLevel(): number {
+  if (
+    ('NODE_ENV' in process.env && process.env.NODE_ENV === 'test') ||
+    ('JEST_WORKER_ID' in process.env &&
+      process.env.JEST_WORKER_ID !== undefined)
+  ) {
+    return error;
+  }
+
+  if ('DEV' in process.env && process.env.DEV === 'true') {
+    return trace;
+  }
+
+  return info;
+}
+
+const minLevel = getLoggingLevel();
 
 export const log = new Logger({ name: 'index', minLevel });
 
