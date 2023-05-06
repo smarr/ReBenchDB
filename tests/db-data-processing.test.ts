d2051c02546461fe366ec0d6e302fa89600911fa
Stefan Marr <git@stefan-marr.de>
2023-05-06 23:46:57 +0100

    Add also test for the TruffleSOM data set

--- tests/db-data-processing.test.ts
+++ tests/db-data-processing.test.ts
@@ -98,4 +98,81 @@ describe('collateMeasurements()', () => {
       expect(m2.values[0][9]).toEqual(78.637);
     });
   });
+
+  describe('with data from TruffleSOM', () => {
+    const result: ResultsByExeSuiteBenchmark = collateMeasurements(
+      dataTSOM.results
+    );
+
+    it('should have 7 executors', () => {
+      expect(result.size).toBe(7);
+      expect([...result.keys()]).toEqual([
+        'TruffleSOM-graal',
+        'TruffleSOM-interp',
+        'TruffleSOM-graal-bc',
+        'TruffleSOM-native-interp-bc',
+        'TruffleSOM-native-interp-ast',
+        'SomSom-native-interp-ast',
+        'SomSom-native-interp-bc'
+      ]);
+    });
+
+    it('should have 2 suites', () => {
+      const suites = <ResultsBySuiteBenchmark>result.get('TruffleSOM-graal');
+      expect(suites.size).toBe(4);
+      expect([...suites.keys()]).toEqual([
+        'macro-steady',
+        'micro-steady',
+        'macro-startup',
+        'micro-startup'
+      ]);
+    });
+
+    it('should have the expected macro benchmarks', () => {
+      const suites = <ResultsBySuiteBenchmark>result.get('TruffleSOM-graal');
+      const macro = <ResultsByBenchmark>suites.get('macro-steady');
+      expect(macro.benchmarks.size).toBe(6);
+
+      expect([...macro.benchmarks.keys()]).toEqual([
+        'Json',
+        'Richards',
+        'NBody',
+        'DeltaBlue',
+        'GraphSearch',
+        'PageRank'
+      ]);
+    });
+
+    it('should have the expected high-level details for NBody', () => {
+      const suites = <ResultsBySuiteBenchmark>result.get('TruffleSOM-graal');
+      const macro = <ResultsByBenchmark>suites.get('macro-steady');
+      const nbody = <ProcessedResult>macro.benchmarks.get('NBody');
+
+      expect(nbody.bench).toEqual('NBody');
+      expect(nbody.exe).toEqual('TruffleSOM-graal');
+      expect(nbody.suite).toEqual('macro-steady');
+
+      expect(nbody.measurements).toHaveLength(2);
+    });
+
+    it('should have the expected measurements for NBody', () => {
+      const suites = <ResultsBySuiteBenchmark>result.get('TruffleSOM-graal');
+      const macro = <ResultsByBenchmark>suites.get('macro-steady');
+      const nbody = <ProcessedResult>macro.benchmarks.get('NBody');
+
+      const m1 = nbody.measurements[0];
+      const m2 = nbody.measurements[1];
+
+      expect(m1.commitId).toEqual('5820ec');
+      expect(m2.commitId).toEqual('5fa4bd');
+      expect(m1.criterion).toEqual({ name: 'total', unit: 'ms' });
+      expect(m2.criterion).toEqual({ name: 'total', unit: 'ms' });
+
+      expect(m1.values).toHaveLength(1);
+      expect(m2.values).toHaveLength(1);
+
+      expect(m1.values[0]).toHaveLength(120);
+      expect(m2.values[0]).toHaveLength(120);
+    });
+  });
 });
