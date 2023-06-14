b813b6e532e34d72493021d4bb5e332d6fc09073
Stefan Marr <git@stefan-marr.de>
2023-06-14 10:51:24 +0100

    Fix paths after moving code around

--- src/backend/dev-server/server.ts
+++ src/backend/dev-server/server.ts
@@ -1,6 +1,8 @@
 import { ParameterizedContext } from 'koa';
 import { readFileSync } from 'node:fs';
+
 import { log } from '../../logging.js';
+import { robustPath, robustSrcPath } from '../../util.js';
 
 export async function serveStaticResource(
   ctx: ParameterizedContext
@@ -12,24 +14,24 @@ export async function serveStaticResource(
   // TODO: robustPath?
   if (filename.endsWith('.css')) {
     ctx.type = 'css';
-    path = `${__dirname}/../../resources/${filename}`;
+    path = robustPath(`../resources/${filename}`);
   } else if (filename.endsWith('.js')) {
     ctx.type = 'application/javascript';
     if (filename.includes('uPlot')) {
-      path = `${__dirname}/../../resources/${filename}`;
+      path = robustPath(`../resources/${filename}`);
     } else {
-      path = `${__dirname}/views/${filename}`;
+      path = robustSrcPath(`views/${filename}`);
     }
   } else if (filename.endsWith('.map')) {
     ctx.type = 'application/json';
-    path = `${__dirname}/views/${filename}`;
+    path = robustSrcPath(`views/${filename}`);
   } else if (filename.endsWith('.svg')) {
     ctx.type = 'image/svg+xml';
-    path = `${__dirname}/../../resources/${filename}`;
+    path = robustPath(`../resources/${filename}`);
   } else if (filename.endsWith('.json.gz')) {
     ctx.type = 'application/json';
     ctx.set('Content-Encoding', 'gzip');
-    path = `${__dirname}/../../resources/${filename}`;
+    path = robustPath(`../resources/${filename}`);
   } else {
     throw new Error(`Unsupported file type. Filename: ${filename}`);
   }
@@ -41,7 +43,7 @@ export async function serveViewJs(ctx: ParameterizedContext): Promise<void> {
   let path: string;
   if (ctx.params.filename.endsWith('.ts')) {
     ctx.type = 'application/typescript';
-    path = `${__dirname}/../../src/views/${ctx.params.filename}`;
+    path = robustPath(`views/${ctx.params.filename}`);
   } else {
     throw new Error(`Unsupported file type ${ctx.params.filename}`);
   }
@@ -50,7 +52,7 @@ export async function serveViewJs(ctx: ParameterizedContext): Promise<void> {
 
 export async function serveReport(ctx: ParameterizedContext): Promise<void> {
   log.debug(`serve ${ctx.params.filename}`);
-  const reportPath = `${__dirname}/../../resources/reports`;
+  const reportPath = robustPath(`../resources/reports`);
   ctx.body = readFileSync(
     `${reportPath}/${ctx.params.change}/figure-html/${ctx.params.filename}`
   );
