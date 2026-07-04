e09d5b2c7c7423a4e80f913ef004c006fbc7d474
Stefan Marr <git@stefan-marr.de>
2026-07-04 14:21:37 +0200

    Remove unused respond*IdNotFound
    - project ids are no longer exposed
    
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/backend/common/standard-responses.ts
+++ src/backend/common/standard-responses.ts
@@ -1,14 +1,5 @@
 import { ParameterizedContext } from 'koa';
 
-export function respondProjectIdNotFound(
-  ctx: ParameterizedContext,
-  projectId: number
-): void {
-  ctx.body = `Requested project with id ${projectId} not found`;
-  ctx.status = 404;
-  ctx.type = 'text';
-}
-
 export function respondProjectNotFound(
   ctx: ParameterizedContext,
   projectSlug: string
@@ -26,15 +17,6 @@ export function respondProjectAndSourceNotFound(
   ctx.body =
     `Requested combination of project "${projectSlug}"` +
     ` and source ${sourceId} not found`;
-  ctx.status = 404;
-  ctx.type = 'text';
-}
-
-export function respondExpIdNotFound(
-  ctx: ParameterizedContext,
-  expId: string
-): void {
-  ctx.body = `Requested experiment ${expId} not found`;
   ctx.status = 404;
   ctx.type = 'text';
 }
