09f474680475a6f0eb940400990e3a563df0a14f
Stefan Marr <git@stefan-marr.de>
2026-09-08 15:14:04 +0200

    Add and document handling for other status in isJobActive
    Signed-off-by: Stefan Marr <git@stefan-marr.de>

--- src/backend/gitlab/runner-status.ts
+++ src/backend/gitlab/runner-status.ts
@@ -322,11 +322,10 @@ const passwordPageTpl = prepareTemplate(
 );
 
 function isJobActive(job: Job) {
-  return (
-    job.status == 'RUNNING' ||
-    job.status == 'PENDING' ||
-    job.status == 'CREATED'
-  );
+  // Note, this does not contain everything that's requested from GitLab.
+  // SCHEDULED, WAITING_FOR_CALLBACK, WAITING_FOR_RESOURCE are not included,
+  // because they are not necessarily depending on running jobs.
+  return ['RUNNING', 'PENDING', 'PREPARING', 'CREATED'].includes(job.status);
 }
 
 export function createGraphQLClient(): GraphQLClient {
